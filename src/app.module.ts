e7b7b634dc2c66e9d993667c4fe753d30b350468
paulmkvts <morgan855873@gmail.com>
2021-11-06 16:00:04 +0300

    Added modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule,
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'qwerty',
+    entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+    synchronize: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
