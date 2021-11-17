6743137bdb691e01e65f38f390b6c17166061c0f
paulmkvts <morgan855873@gmail.com>
2021-11-17 18:30:59 +0300

    unit-тест для UsersService

--- src/users/tests/users.service.spec.ts
+++ src/users/tests/users.service.spec.ts
@@ -3,8 +3,7 @@ import { User, UsersRepositoryFake } from '../user.entity';
 import { Test } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import faker from 'faker';
-import exp from 'constants';
+import { HttpException } from '@nestjs/common';
 
 describe('UsersService', () => {
   let usersService: UsersService;
@@ -42,6 +41,41 @@ describe('UsersService', () => {
       expect(usersRepositoryCreateSpy).toBeCalledWith(createUserData);
       expect(usersRepositorySaveSpy).toBeCalledWith(savedUserData);
       expect(result).toEqual(savedUserData);
+    });
+  });
+  describe('when getting user by email', () => {
+    it('throws an error when no user exists', async () => {
+      const randomEmail = 'test@test.com';
+
+      const usersRepositoryFindOneSpy = jest
+        .spyOn(usersRepository, 'findOne')
+        .mockResolvedValue(null);
+      expect.assertions(3);
+      try {
+        await usersService.getByEmail(randomEmail);
+      } catch (e) {
+        expect(e).toBeInstanceOf(HttpException);
+        expect(e.message).toBe('User with this email does not exist');
+      }
+      expect(usersRepositoryFindOneSpy).toHaveBeenCalledWith({
+        email: randomEmail,
+      });
+    });
+    it('should return user', async () => {
+      const randomEmail = 'test@test.com';
+      const existingUser = {
+        id: 123,
+        email: randomEmail,
+        password: 'testtest123',
+      };
+      const usersRepositoryFindOneSpy = jest
+        .spyOn(usersRepository, 'findOne')
+        .mockResolvedValue(existingUser);
+      const result = await usersService.getByEmail(randomEmail);
+      expect(result).toBe(existingUser);
+      expect(usersRepositoryFindOneSpy).toHaveBeenCalledWith({
+        email: randomEmail,
+      });
     });
   });
 });
